8c55e5ebb5104ba8643d391f8b6d6df0593ecc57
CleberGomesJr <clebergomes2051@gmail.com>
2025-03-25 15:17:47 -0300

    refactor: Refatoração da classe Receita com validações e ID único

--- src/ControleFinanceiro/Models/Receita.cs
+++ src/ControleFinanceiro/Models/Receita.cs
@@ -2,15 +2,21 @@ using System;
 
 namespace ControleFinanceiro.Models{
     public class Receita{
-        public string Nome{get; set;};
-        public decimal Valor{get; set;};
-        public DateTime Data{set;};
+        public static int _idCounter = 1;
+        public int Id{get;}
+        public string Descricao{ get; set; }
+        public decimal Valor {get; set; }
+        public string Categoria{get; set; }
+        public DateTime Data {get; set; }
 
-        public Receita(string nome, decimal valor){
-            if (valor <= 0)
-                throw new ArgumentException("O valor de uma receita não pode ser negativo.");
-            Nome = nome;
+        public Receita(string descricao, decimal valor, string categoria){
+            if(valor <= 0)
+                throw new ArgumentException("O valor da Receita não pode ser negativo ou nulo.");
             Valor = valor;
+            Categoria = categoria;
+            Descricao = descricao;
+            Id = _idCounter++;
+            Data = DateTime.Today;
         }
     }
 }
