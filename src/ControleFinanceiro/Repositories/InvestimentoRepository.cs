05a4204ffb746d62ff3c6f6135ef0015d648698c
CleberGomesJr <clebergomes2051@gmail.com>
2025-03-31 15:48:56 -0300

    fix: fix em erros de digitação e lógica

--- src/ControleFinanceiro/Repositories/InvestimentoRepository.cs
+++ src/ControleFinanceiro/Repositories/InvestimentoRepository.cs
@@ -2,22 +2,33 @@ using ControleFinanceiro.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ControleFinanceiro.Repositories{
-    public class InvestimentoRepository{
-        private statis List<Investimento> _investimentos = new Investimento();
-        public void AdicionarInvestimento(Investimento investimento){
-            _investimetos.Add = investimento;
+namespace ControleFinanceiro.Repositories
+{
+    public class InvestimentoRepository
+    {
+        private static List<Investimento> _investimentos = new List<Investimento>();
+
+        public void AdicionarInvestimento(Investimento investimento)
+        {
+            _investimentos.Add(investimento);
         }
-        public List<Investimento> ListarInvestimentos(){
+
+        public List<Investimento> ListarInvestimentos()
+        {
             return _investimentos;
         }
-        public Investimento BuscaPorId(int id){
-            return _investimentos.FirstOrDefault(r => r.Id == id)
+
+        public Investimento BuscarPorId(int id)
+        {
+            return _investimentos.FirstOrDefault(r => r.Id == id);
         }
-        public void RemoverInvestimento(int id){
-            var investimento = BuscaPorId(id);
-            if (investimento !+ null){
-                _investimento.Remove(investimento);
+
+        public void RemoverInvestimento(int id)
+        {
+            var investimento = BuscarPorId(id);
+            if (investimento != null)
+            {
+                _investimentos.Remove(investimento);
             }
         }
     }
