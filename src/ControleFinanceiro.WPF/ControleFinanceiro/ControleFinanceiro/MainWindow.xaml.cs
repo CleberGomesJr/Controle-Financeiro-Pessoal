e98495ea0ee5474f090450d0af2813568d5a4ce1
CleberGomesJr <clebergomes2051@gmail.com>
2025-04-02 14:35:49 -0300

    feat: criação da base da interface do usuário

---
+++ src/ControleFinanceiro.WPF/ControleFinanceiro/ControleFinanceiro/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControleFinanceiroWPF
+{
+    public partial class MainWindow : Window
+    {
+        public decimal TotalReceitas { get; set; } = 5000m;
+        public decimal TotalDespesas { get; set; } = 2000m;
+        public decimal TotalInvestimentos { get; set; } = 1500m;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this; // Liga os dados à interface
+        }
+    }
+}
