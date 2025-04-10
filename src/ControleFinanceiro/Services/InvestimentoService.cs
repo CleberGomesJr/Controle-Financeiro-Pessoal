4562836883cb4c31f8cee0ff8eaa3f9c43e07db8
CleberGomesJr <clebergomes2051@gmail.com>
2025-04-10 10:37:22 -0300

    refeat: Refazendo a lógica dos Services para armazenagem

--- src/ControleFinanceiro/Services/InvestimentoService.cs
+++ src/ControleFinanceiro/Services/InvestimentoService.cs
@@ -1,40 +1,28 @@
+using ControleFinanceiro.Data;
 using ControleFinanceiro.Models;
-using ControleFinanceiro.Repositories;
 using System.Collections.Generic;
 
 namespace ControleFinanceiro.Services
 {
     public class InvestimentoService
     {
-        private readonly InvestimentoRepository _investimentoRepository;
+        private List<Investimento> _investimentos;
 
         public InvestimentoService()
         {
-            _investimentoRepository = new InvestimentoRepository();
+            _investimentos = DataStorage.CarregarInvestimentos();
         }
 
-        public void AdicionarInvestimento(string nome, decimal valor)
+        public void AdicionarInvestimento(string nomeAtivo, decimal valor)
         {
-            if (valor <= 0)
-                throw new ArgumentException("O valor do investimento deve ser positivo.");
-
-            var investimento = new Investimento(nome, valor);
-            _investimentoRepository.AdicionarInvestimento(investimento);
+            var investimento = new Investimento(nomeAtivo, valor);
+            _investimentos.Add(investimento);
+            DataStorage.SalvarInvestimentos(_investimentos);
         }
 
         public List<Investimento> ListarInvestimentos()
         {
-            return _investimentoRepository.ListarInvestimentos();
-        }
-
-        public Investimento BuscarPorId(int id)
-        {
-            return _investimentoRepository.BuscarPorId(id);
-        }
-
-        public void RemoverInvestimento(int id)
-        {
-            _investimentoRepository.RemoverInvestimento(id);
+            return _investimentos;
         }
     }
 }
--- src/ControleFinanceiro/Services/ReceitaService.cs
+++ src/ControleFinanceiro/Services/ReceitaService.cs
@@ -1,30 +1,28 @@
+using ControleFinanceiro.Data;
 using ControleFinanceiro.Models;
-using ControleFinanceiro.Repositories;
 using System.Collections.Generic;
 
 namespace ControleFinanceiro.Services
 {
-    public class ReceitaService{
-        private readonly ReceitaRepository _receitaRepository;
+    public class ReceitaService
+    {
+        private List<Receita> _receitas;
 
-        public ReceitaService(){
-            _receitaRepository = new ReceitaRepository();
-        }
-
-        public void AdicionarReceita(string descricao, decimal valor, string categoria){
-            if (valor <= 0)
-                throw new ArgumentException("O valor da receita deve ser positivo.");
-
-            var receita = new Receita(descricao, valor, categoria);
-            _receitaRepository.AdicionarReceita(receita);
-        }
-
-        public List<Receita> ListarReceitas(){
-            return _receitaRepository.ListarReceitas();
-        }
-        public void RemoverReceita(int id)
+        public ReceitaService()
         {
-            _receitaRepository.RemoverReceita(id);
+            _receitas = DataStorage.CarregarReceitas();
+        }
+
+        public void AdicionarReceita(string descricao, decimal valor, string categoria)
+        {
+            var receita = new Receita(descricao, valor, categoria);
+            _receitas.Add(receita);
+            DataStorage.SalvarReceitas(_receitas);
+        }
+
+        public List<Receita> ListarReceitas()
+        {
+            return _receitas;
         }
     }
 }
